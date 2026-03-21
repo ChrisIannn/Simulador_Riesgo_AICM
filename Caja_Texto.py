cdef1317175d1d65194bb06089ab0e654f3d12fa
ChrisIannn <calf.277@gmail.com>
2026-03-20 18:23:59 -0600

    Add files via upload

---
+++ Caja_Texto.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.title("Simulador de Riesgo AICM")
+
+# Esta es la "Caja manual" que buscabas
+variacion_pax = st.number_input("Introduce la variación del AICM (ej: -0.05 para -5%)", value=0.0)
+
+elasticidad = 2.6782
+impacto = variacion_pax * elasticidad
+
+st.metric(label="Impacto Proyectado en Cotizaciones", value=f"{impacto:.2%}")
