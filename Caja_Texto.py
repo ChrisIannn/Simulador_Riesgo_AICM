d3d6791b5d149a14f09ffd55d25531bb5d20b177
ChrisIannn <calf.277@gmail.com>
2026-03-23 18:14:06 -0600

    Change elasticity value for monthly impact calculation
    Updated the elasticity value to reflect monthly calculations.

--- Caja_Texto.py
+++ Caja_Texto.py
@@ -5,7 +5,9 @@ st.title("Simulador de Riesgo AICM")
 # Esta es la "Caja manual" que buscabas
 variacion_pax = st.number_input("Introduce la variación del AICM (ej: -0.05 para -5%)", value=0.0)
 
-elasticidad = 2.6782
+elasticidad = 0.223183
+# la elasticidad global se dividio entre 12 para representar el correspondiente mensual 
+# elasticidad global = 2.6782
 impacto = variacion_pax * elasticidad
 
 st.metric(label="Impacto Proyectado en Cotizaciones", value=f"{impacto:.2%}")
